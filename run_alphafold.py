45b75c1852b8e8b78dab8862f373632c2b306ebb
Ninjani <janani.durairaj@gmail.com>
2023-07-11 16:53:38 +0200

    fix num_multimer_predictions_per_model

--- run_alphafold.py
+++ run_alphafold.py
@@ -117,14 +117,14 @@ flags.DEFINE_integer('random_seed', None, 'The random seed for the data '
                      'that even if this is set, Alphafold may still not be '
                      'deterministic, because processes like GPU inference are '
                      'nondeterministic.')
-flags.DEFINE_integer('num_predictions_per_model', 5, 'How many '
+flags.DEFINE_integer('num_multimer_predictions_per_model', 5, 'How many '
                      'predictions (each with a different random seed) will be '
                      'generated per model. E.g. if this is 2 and there are 5 '
                      'models then there will be 10 predictions per input. '
                      'Note: this FLAG only applies if model_preset=multimer or dropout=True')
 flags.DEFINE_integer('nstruct_start',1, 'model to start with, can be used to parallelize jobs, '
-                     'e.g --num_predictions_per_model 20 --nstruct_start 20 will only make model _20'
-                     'e.g --num_predictions_per_model 21 --nstruct_start 20 will make model _20 and _21 etc.')
+                     'e.g --num_multimer_predictions_per_model 20 --nstruct_start 20 will only make model _20'
+                     'e.g --num_multimer_predictions_per_model 21 --nstruct_start 20 will make model _20 and _21 etc.')
 flags.DEFINE_boolean('use_precomputed_msas', True, 'Whether to read MSAs that '
                      'have been written to disk instead of running the MSA '
                      'tools. The MSA files are looked up in the output '
@@ -541,7 +541,7 @@ def main(argv):
       use_precomputed_msas=FLAGS.use_precomputed_msas)
 
   if run_multimer_system:
-    num_predictions_per_model = FLAGS.num_predictions_per_model
+    num_multimer_predictions_per_model = FLAGS.num_multimer_predictions_per_model
     data_pipeline = pipeline_multimer.DataPipeline(
         monomer_data_pipeline=monomer_data_pipeline,
         jackhmmer_binary_path=FLAGS.jackhmmer_binary_path,
@@ -549,9 +549,9 @@ def main(argv):
         use_precomputed_msas=FLAGS.use_precomputed_msas)
   else:
     if FLAGS.dropout:
-      num_predictions_per_model = FLAGS.num_predictions_per_model
+      num_multimer_predictions_per_model = FLAGS.num_multimer_predictions_per_model
     else:
-      num_predictions_per_model = 1
+      num_multimer_predictions_per_model = 1
     data_pipeline = monomer_data_pipeline
 
   model_runners = {}
@@ -585,7 +585,7 @@ def main(argv):
     model_params = data.get_model_haiku_params(
         model_name=model_name, data_dir=FLAGS.data_dir)
     model_runner = model.RunModel(model_config, model_params, is_training=FLAGS.dropout)
-    for i in range(FLAGS.nstruct_start, num_predictions_per_model + 1):
+    for i in range(FLAGS.nstruct_start, num_multimer_predictions_per_model + 1):
       model_runners[f'{model_name}_{FLAGS.suffix}_{i}'] = model_runner
 
   logging.info('Have %d models: %s', len(model_runners),
